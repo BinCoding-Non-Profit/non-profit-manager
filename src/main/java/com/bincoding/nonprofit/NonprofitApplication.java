97686c9a514009c3a0011a547298fa8d4e2e4185
Bilal Siddiqui <bsiddiqui@lmbsiddiq061029.local>
2020-04-11 11:39:25 -0400

    add swagger to project

--- src/main/java/com/bincoding/nonprofit/NonprofitApplication.java
+++ src/main/java/com/bincoding/nonprofit/NonprofitApplication.java
@@ -2,8 +2,10 @@ package com.bincoding.nonprofit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class NonprofitApplication {
 
 	public static void main(String[] args) {
